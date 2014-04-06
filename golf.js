c4c8b19332b00d46d2a01e64efd481ecf9c3d20e
Mike Swartz <mswartz@gmail.com>
2014-04-05 21:32:33 -0400

    got updating working, holy cow

--- golf.js
+++ golf.js
@@ -126,7 +126,43 @@ if (Meteor.isClient) {
       Session.set('editmode', true);
     },
     'click #update_course' : function(){
-      alert('updated!');
+
+      var Course = {};
+      var holes = [];
+      var out_tot = 0;
+      var in_tot = 0;
+      var course_id = Courses.findOne({'name':Session.get('courseSelected')})._id;
+
+      Course.name = $('#course_name').html();
+
+      for(var i=1; i<19; i++){
+        var par = ".hole-"+i+"-par";
+        var hcp = ".hole-"+i+"-hcp";
+
+        holes[i-1] = {
+          'num' : i,
+          'par' : parseInt($(par).html()),
+          'hcp' : parseInt($(hcp).html())
+        }
+      }
+
+      for(var i = 0; i<9; i++){
+        out_tot = out_tot + holes[i].par;
+      }
+
+      for(var i = 9; i<18; i++){
+        in_tot = in_tot + holes[i].par;
+      }
+
+      Course.holes = holes;
+      Course.out_tot = out_tot;
+      Course.in_tot = in_tot;
+      Course.tot = out_tot + in_tot;
+      console.log(course_id);
+      console.log(Course);
+
+      Meteor.call('updateCourse', course_id, Course);
+
       Session.set('editmode', false);
     },
     'click #delete_course' : function(){
--- methods.js
+++ methods.js
@@ -5,6 +5,16 @@ Meteor.methods({
 	'removeCourse' : function(course){
 		Courses.remove(course);
 	},
+	'updateCourse' : function(id, course){
+		Courses.update(id, {$set:
+			{'name': course.name,
+			'holes': course.holes,
+			'out_tot':course.out_tot,
+			'in_tot':course.in_tot,
+			'tot':course.tot}},
+			{multi : true}
+		);
+	},
 	'addRound' : function(){
 		//add a round
 	}
